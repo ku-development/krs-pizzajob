4def6a9a08d7862cdbebf08bcd0647a4eafebd22
KU Development <106535297+ku-development@users.noreply.github.com>
2024-01-12 21:53:07 +0200

    updated exp system now works as it should

--- server/main.lua
+++ server/main.lua
@@ -7,20 +7,14 @@ AddEventHandler('krs-pizzajob:server:deliverPizza', function(deliveryamount, pay
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     if Config.XP.Use then
-        TriggerServerEvent('krs-pizzajob:server:EXP', deliveryamount, payoutAmount)
-    else
-        Player.Functions.AddMoney(Config.PayCheckType, payoutAmount)
-    end
-end)
-
-RegisterNetEvent('krs-pizzajob:server:EXP')
-AddEventHandler('krs-pizzajob:server:EXP', function(deliveryamount, payoutAmount)
-        local src = source
-        local exp = getxp(src)
+        local exp = getxp(source)
         local pData = QBCore.Functions.GetPlayer(src)
         local PizzaExp = pData.PlayerData.metadata[MetaDataName]
         pData.Functions.SetMetaData(MetaDataName, (PizzaExp + exp)) -- Edit xp reward here
         ServerNotify('You are now '.. getLevel(src) .. ' Level and you have '.. PizzaExp .. " EXP", 'info', src)
+    else
+        Player.Functions.AddMoney(Config.PayCheckType, payoutAmount)
+    end
 end)
 
 RegisterNetEvent("krs-pizzajobL:server:Fail")
