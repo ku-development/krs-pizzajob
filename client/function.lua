462939c2b18e68697558076c52350adb88a1ddc1
KU Development <106535297+ku-development@users.noreply.github.com>
2024-01-12 14:08:31 +0200

    improved code added needed functions and anims
    credits - randol pizza job ( animtion was taken from there )

--- client/function.lua
+++ client/function.lua
@@ -1,28 +1,32 @@
 local QBCore = exports[Config.CoreResourceName]:GetCoreObject()
+local Hired = false
+local HasPizza = false
+local DeliveriesCount = 0
+local Delivered = false
+local PizzaDelivered = false
+local ownsVan = false
+local activeOrder = false
+local JobBlip = nil
+local newDelivery = {}
+
 -- Function to send notifications
 function Notify(text)
-    -- Check if QB notifications are enabled
- if Config.Notify == 'qb' then
-        -- Send QB notification
+    if Config.Notify == 'qb' then
         QBCore.Functions.Notify(text)
-    -- Check if okok notifications are enabled
- elseif Config.Notify == 'okok' then
-        -- Send okok notification
+    elseif Config.Notify == 'okok' then
         exports['okoknotify']:Alert('PizzaJob Information', text, 5000, 'info')
-    -- Check if ox notifications are enabled
     elseif Config.Notify == 'ox' then
-        -- Send ox notification
         lib.notify({
-            -- Notification title
             title = 'Information',
-            -- Notification description
             description = text,
-            -- Notification type
             type = 'inform',
-            -- Notification position
             position = 'top',
         })
     end
+end
+
+function StartJob()
+    PullOutVehicle()
 end
 
 function NextDelivery()
@@ -40,9 +44,101 @@ function NextDelivery()
         BeginTextCommandSetBlipName("STRING")
         AddTextComponentSubstringPlayerName("Next Customer")
         EndTextCommandSetBlipName(JobBlip)
-        exports['qb-target']:AddCircleZone("deliverZone", vector3(newDelivery.x, newDelivery.y, newDelivery.z), 1.3,{ name = "deliverZone", debugPoly = false, useZ=true, }, { options = { { type = "client", event = "randol_pizzajob:client:deliverPizza", icon = "fa-solid fa-pizza-slice", label = "Deliver Pizza"}, }, distance = 1.5 })
+        exports['qb-target']:AddCircleZone("deliverZone", vector3(newDelivery.x, newDelivery.y, newDelivery.z), 1.3, { name = "deliverZone", debugPoly = false, useZ=true, }, { options = { { type = "client", event = "randol_pizzajob:client:deliverPizza", icon = "fa-solid fa-pizza-slice", label = "Deliver Pizza" }, }, distance = 1.5 })
         activeOrder = true
         Notify('You have a new delivery!')
-       
+    end
+end
+
+function PullOutVehicle()
+    local coords = Config.VehicleSpawn
+    QBCore.Functions.SpawnVehicle(Config.Vehicle, function(pizzaCar)
+        SetVehicleNumberPlateText(pizzaCar, "PIZZA" .. tostring(math.random(1000, 9999)))
+        SetVehicleColours(pizzaCar, 111, 111)
+        SetVehicleDirtLevel(pizzaCar, 1)
+        DecorSetFloat(pizzaCar, "pizza_job", 1)
+        TaskWarpPedIntoVehicle(PlayerPedId(), pizzaCar, -1)
+        TriggerEvent("vehiclekeys:client:SetOwner", GetVehicleNumberPlateText(pizzaCar))
+        SetVehicleEngineOn(pizzaCar, true, true)
+        exports[Config.FuelScript]:SetFuel(pizzaCar, 100.0)
+        exports['qb-target']:AddTargetEntity(pizzaCar, {
+            options = {
+                {
+                    icon = "fa-solid fa-pizza-slice",
+                    label = "Take Pizza",
+                    action = function(entity) TakePizza() end,
+                    canInteract = function() 
+                        return Hired and activeOrder and not HasPizza
+                    end,
+                },
+            },
+            distance = 2.5
+        })
+    end, coords, true)
+    Hired = true
+    ownsVan = true
+    NextDelivery()
+end
+
+function loadAnimDict(dict)
+    while not HasAnimDictLoaded(dict) do
+        RequestAnimDict(dict)
+        Wait(0)
+    end
+end
+
+function TakePizza()
+    local player = PlayerPedId()
+    local pos = GetEntityCoords(player)
+    
+    if not IsPedInAnyVehicle(player, false) then
+        local ad = "anim@heists@box_carry@"
+        local prop_name = 'prop_pizza_box_01'
+
+        if DoesEntityExist(player) and not IsEntityDead(player) then
+            if not HasPizza then
+                if #(pos - vector3(newDelivery.x, newDelivery.y, newDelivery.z)) < 30.0 then
+                    loadAnimDict(ad)
+                    local x, y, z = table.unpack(GetEntityCoords(player))
+                    prop = CreateObject(GetHashKey(prop_name), x, y, z+0.2, true, true, true)
+                    AttachEntityToEntity(prop, player, GetPedBoneIndex(player, 60309), 0.2, 0.08, 0.2, -45.0, 290.0, 0.0, true, true, false, true, 1, true)
+                    TaskPlayAnim(player, ad, "idle", 3.0, -8, -1, 63, 0, 0, 0, 0)
+                    HasPizza = true
+                else
+                    Notify("You're not close enough to the customer's house!")
+                end
+            end
+        end
+    end
+end
+
+function FinishWork()
+    local ped = PlayerPedId()
+    local pos = GetEntityCoords(ped)
+    local veh = QBCore.Functions.GetClosestVehicle()
+    local finishspot = vector3(Config.JobStartLocation.x, Config.JobStartLocation.y, Config.JobStartLocation.z)
+    
+    if #(pos - finishspot) < 10.0 then
+        if Hired then
+            if DecorExistOn((veh), "pizza_job") then
+                QBCore.Functions.DeleteVehicle(veh)
+                RemoveBlip(JobBlip)
+                Hired = false
+                HasPizza = false
+                ownsVan = false
+                activeOrder = false
+                if DeliveriesCount > 0 then
+                    -- Do something if deliveries were completed
+                else
+                    Notify("You didn't complete any deliveries, so you weren't paid.")
+                    PullOutVehicle()
+                end
+                DeliveriesCount = 0
+            else
+                Notify('You must return your work vehicle to get paid.')
+                PullOutVehicle()
+                return
+            end
+        end
     end
 end
--- client/main.lua
+++ client/main.lua
@@ -1,86 +1,76 @@
 local QBCore = exports[Config.CoreResourceName]:GetCoreObject()
 local npcPed = 0
 
-
-
-
--- Threads
+-- NPC Initialization
 CreateThread(function()
     local npcModel = Config.NpcStart
-    -- Request NPC model
-    RequestModel(npcModel)
 
-    -- Wait until the model is loaded
+    RequestModel(npcModel)
     while not HasModelLoaded(npcModel) do
-    Wait(500)
+        Wait(500)
     end
 
-    -- Set NPC coordinates
     local npcCoords = Config.JobStartLocation
-
-    -- Create NPC ped
     npcPed = CreatePed(4, npcModel, npcCoords.x, npcCoords.y, npcCoords.z, 0.0, false, true)
-
-    -- Make NPC stand still
     TaskStandStill(npcPed, -1)
-
-    -- Set NPC as a mission entity
     SetEntityAsMissionEntity(npcPed, true, true)
-
-    -- Block non-temporary events for NPC
     SetBlockingOfNonTemporaryEvents(npcPed, true)
-
-    -- Make NPC invincible
     SetEntityInvincible(npcPed, true)
 
     -- Add target entity for NPC
     exports[Config.Target]:AddTargetEntity(npcPed, {
-    options = {
-        {
-            num = 1,
-            type = "client",
-            event = "StartJob:Event",  -- Replace with your actual event name
-            icon = 'fas fa-briefcase',  -- Replace with your desired icon
-            label = 'Start Job',  -- Replace with your desired label
-            targeticon = 'fas fa-briefcase',  -- Replace with your desired target icon
-            action = function(entity)
-                -- Implement the logic to start the job here
-                TriggerEvent('krs-pizzajob:client:StartJob')
-            end,
-            canInteract = function(entity, distance, data)
-                -- Add any additional conditions for interaction here
-                return true
-            end,
-        }
-    },
-    distance = 2.5,
+        options = {
+            {
+                num = 1,
+                type = "client",
+                event = "krs-pizzajob:client:StartJob",
+                icon = 'fas fa-briefcase',
+                label = 'Start Job',
+                targeticon = 'fas fa-briefcase',
+                action = function(entity)
+                    TriggerEvent('krs-pizzajob:client:StartJob')
+                end,
+                canInteract = function(entity, distance, data)
+                    return true
+                end,
+            }
+        },
+        distance = 2.5,
     })
 end)
 
--- Events
+-- Decor Registration Thread
+CreateThread(function()
+    DecorRegister("pizza_job", 1)
+end)
+
+-- Pizza Job Blip
+CreateThread(function()
+    local pizzajobBlip = AddBlipForCoord(vector3(Config.JobStartLocation.x, Config.JobStartLocation.y, Config.JobStartLocation.z)) 
+    SetBlipSprite(pizzajobBlip, 267)
+    SetBlipAsShortRange(pizzajobBlip, true)
+    SetBlipScale(pizzajobBlip, 0.8)
+    SetBlipColour(pizzajobBlip, 2)
+    BeginTextCommandSetBlipName("STRING")
+    AddTextComponentString("Pizza Job")
+    EndTextCommandSetBlipName(pizzajobBlip)
+end)
+
+-- Event: Start Job
 RegisterNetEvent('krs-pizzajob:client:StartJob', function()
-   -- Get job vehicle model from config
-   local vehicleModel = Config.JobVehicle
+    local vehicleModel = Config.JobVehicle
 
-   -- Request and wait for model to load
-   RequestModel(vehicleModel)
-   while not HasModelLoaded(vehicleModel) do
-       Wait(500)
-   end
+    RequestModel(vehicleModel)
+    while not HasModelLoaded(vehicleModel) do
+        Wait(500)
+    end
 
-   -- Get player's ped and create vehicle
-   local playerPed = PlayerPedId()
-   local vehicle = CreateVehicle(vehicleModel, GetEntityCoords(playerPed), GetEntityHeading(playerPed), true, false)
+    local playerPed = PlayerPedId()
+    local vehicle = CreateVehicle(vehicleModel, GetEntityCoords(playerPed), GetEntityHeading(playerPed), true, false)
 
-   -- Warp player into vehicle
-   TaskWarpPedIntoVehicle(playerPed, vehicle, -1)
+    TaskWarpPedIntoVehicle(playerPed, vehicle, -1)
+    Wait(1000)
 
-   -- Wait to prevent bugs
-   Wait(1000)
-
-   -- Set vehicle owner
-   TriggerEvent("vehiclekeys:client:SetOwner", GetVehicleNumberPlateText(vehicle), vehicle)
-
-   -- SetPlayerInJob
-   NextDelivery()
+    TriggerEvent("vehiclekeys:client:SetOwner", GetVehicleNumberPlateText(vehicle), vehicle)
+    StartJob()
 end)
